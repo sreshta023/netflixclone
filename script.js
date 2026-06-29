c2c3a464ebb4e86192e353dc3241055da0aaec0f
Sreshta <sreshtapasunooru@gmail.com>
2026-06-29 12:45:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+        function signin() {
+            document.getElementById("landingPage").style.display = "none";
+            document.getElementById("homePage").style.display = "block";
+            window.scrollTo(0,0);
+        }
